62ebd2c91994615f0fba54886951f27c23f2746f
Maxuel Lima <maxuellima20@gmail.com>
2023-11-16 02:34:48 -0300

    Avancei na estilização da DetailsPage, cor do fundo, puxar os dados do pokemon

--- projeto-pokedex-krexu/src/Pages/PokemonDetailPage/PokemonDetails.js
+++ projeto-pokedex-krexu/src/Pages/PokemonDetailPage/PokemonDetails.js
@@ -1,83 +1,124 @@
-import React, { useContext, useEffect } from 'react'
-import Header from '../../components/Header/Header'
-import {AddButton, BackButton, ContainerNameDetails, ContainterDetails, HeaderDetails, ImageAside, MainContainer, PokebolaCenter, PokebolaImg, PokemonDetailsStyle, PokemonImage, StatsSection, TopicHeader, TypesAside} from './PokemonDetailsStyle'
-import { useNavigate} from 'react-router-dom'
-import { goBack } from '../../Router/coordinator'
-import pokebola from "../../image/pokebola 2.png"
-import { GlobalContext } from '../../contexts/GlobalContexts'
-import axios from 'axios'
+import React, { useContext, useEffect } from "react";
+import Header from "../../components/Header/Header";
+import {
+  AddButton,
+  AtributesPokemon,
+  BackButton,
+  ContainerDetails,
+  ContainerDetailsImagePokemon,
+  ContainerDetailsNameId,
+  ContainerDetailsTop,
+  ContainerMoves,
+  ContainerNameDetails,
+  DetailsMoves,
+  HeaderDetails,
+  ImageAside,
+  MainContainer,
+  PokebolaCenter,
+  PokebolaImage,
+  PokebolaImg,
+  PokemonDetailsStyle,
+  PokemonImage,
+  StatsSection,
+  TopicHeader,
+  TypesAside,
+} from "./PokemonDetailsStyle";
+import { useNavigate } from "react-router-dom";
+import { goBack } from "../../Router/coordinator";
+import pokebola from "../../image/pokebola 2.png";
+import { GlobalContext } from "../../contexts/GlobalContexts";
 
 const PokemonDetails = () => {
+  const navigate = useNavigate();
 
-  const navigate = useNavigate()
+  const { pokeDetails} = useContext(GlobalContext);
 
-  const { pokeDetails, setPokeDetails} = useContext(GlobalContext);
-  
+  console.log(pokeDetails);
 
-  console.log(pokeDetails)
-
-  // const dataDetailsPokemons = async () =>{
-  //   try{
-  //     const response = await axios.get(pokeDetails.url);
-  //     setPokeDetails(response.data)      
-  //   } catch(error){
-  //     console.log('Error', error);
-  //   }
-  // }
-  // //Renderizar na tela
-  // useEffect(()=>{
-  //   dataDetailsPokemons();
-  // },[pokeDetails])
+  const type = pokeDetails.types && pokeDetails.types[0].type.name
 
   return (
     <PokemonDetailsStyle>
       <HeaderDetails>
-      <BackButton onClick={()=>goBack(navigate)}> {`<`}Todos os Pokemons</BackButton>
-      <Header/>
-      <AddButton>Adicionar/excluir da Pokedex</AddButton>
+        <BackButton onClick={() => goBack(navigate)}>
+          {" "}
+          {`<`}Todos os Pokemons
+        </BackButton>
+        <Header />
+        <AddButton>Adicionar/excluir da Pokedex</AddButton>
       </HeaderDetails>
       <MainContainer>
-      <PokebolaCenter src={pokebola} alt="pokebola transparente" />
+        <PokebolaCenter src={pokebola} alt="pokebola transparente" />
         <ContainerNameDetails>
           <h1>Detalhes</h1>
         </ContainerNameDetails>
-      <ContainterDetails>
-      <PokebolaImg src={pokebola} alt="pokebola transparente" />
-      <ImageAside>
-        <PokemonImage><img src={pokeDetails.sprites?.front_default} alt="" /></PokemonImage>
-        <PokemonImage></PokemonImage>
-      </ImageAside>
-      <StatsSection>
-        <TopicHeader><h4>Stats</h4></TopicHeader>
-        <div>
-        <p>HP:{pokeDetails.name}</p>
-        <p>Attack:</p>
-        <p>Defense:</p>
-        <p>Special-ataque:</p>
-        <p>Special-defense:</p>
-        <p>Speed:</p>
-        </div>
-      </StatsSection>
-      <TypesAside>
-        <div>
-          <h4>Type 1 </h4>
-          <h4>Type 2 </h4>
-        </div>
-      <div><h4>Moves</h4></div>
-        <div>
-        <p>HP:</p>
-        <p>Attack:</p>
-        <p>Defense:</p>
-        <p>Special-ataque:</p>
-        <p>Special-defense:</p>
-        <p>Speed:</p>
-        </div>
-      </TypesAside>
-      </ContainterDetails>
-      </MainContainer>
-      
-    </PokemonDetailsStyle>
-  )
-}
+        <ContainerDetails type={type}>
+          <PokebolaImg src={pokebola} alt="pokebola transparente" />
+          <ImageAside>
+            <PokemonImage>
+              <img
+                src={pokeDetails.sprites?.front_default}
+                alt="Pokemon de frente"
+              />
+            </PokemonImage>
+            <PokemonImage>
+              {" "}
+              <img
+                src={pokeDetails.sprites?.back_default}
+                alt="Pokemon de frente"
+              />
+            </PokemonImage>
+          </ImageAside>
+          <StatsSection>
+            <TopicHeader>
+              <h2>Base Stats</h2>
+            </TopicHeader>
+            <AtributesPokemon>
+              <p>HP {pokeDetails.stats[0]?.base_stat}</p>
+              <p>Attack {pokeDetails.stats[1]?.base_stat}</p>
+              <p>Defense {pokeDetails.stats[2]?.base_stat} </p>
+              <p>Sp.Atk {pokeDetails.stats[3]?.base_stat} </p>
+              <p>Sp.Def {pokeDetails.stats[4]?.base_stat} </p>
+              <p>Speed {pokeDetails.stats[5]?.base_stat} </p>
+              <p>Total</p>
+            </AtributesPokemon>
+          </StatsSection>
+          <TypesAside>
+            <ContainerDetailsTop>
+              <ContainerDetailsNameId>
+                <p>
+                  #{pokeDetails.id < 10 ? `0${pokeDetails.id}` : pokeDetails.id}
+                </p>
+                <h2>{pokeDetails.name}</h2>
+              </ContainerDetailsNameId>
 
-export default PokemonDetails
+              <ContainerDetailsImagePokemon>
+                <img
+                  src={
+                    pokeDetails.sprites?.other["official-artwork"].front_default
+                  }
+                  alt={pokeDetails.name}
+                />
+              </ContainerDetailsImagePokemon>
+            </ContainerDetailsTop>
+            <ContainerMoves>
+              <DetailsMoves>
+              <h2>Moves:</h2>
+              <div>
+              <p>{pokeDetails.moves[0]?.move.name}</p>
+              <p>{pokeDetails.moves[1]?.move.name}</p>
+              <p>{pokeDetails.moves[2]?.move.name}</p>
+              <p>{pokeDetails.moves[3]?.move.name}</p>
+              <p>{pokeDetails.moves[4]?.move.name}</p>
+              </div>
+            </DetailsMoves>
+            </ContainerMoves>
+            
+          </TypesAside>
+        </ContainerDetails>
+      </MainContainer>
+    </PokemonDetailsStyle>
+  );
+};
+
+export default PokemonDetails;
--- projeto-pokedex-krexu/src/Pages/PokemonDetailPage/PokemonDetailsStyle.js
+++ projeto-pokedex-krexu/src/Pages/PokemonDetailPage/PokemonDetailsStyle.js
@@ -1,6 +1,27 @@
 import React from "react";
 import styled from "styled-components";
 
+const getBackground = {
+  water: "#71C3FF",
+  bug: "#76A866",
+  dark: "#A79FA6",
+  dragon: "#004170",
+  electric: "#F2E7AA",
+  fairy: "#E9CDEA",
+  fighting: "#D88EAB",
+  fire: "#EAAB7D",
+  flying: "#A4B8C9",
+  ghost: "#9B9FB9",
+  grass: "#729F92",
+  ground: "#E1A48A",
+  ice: "#B5DCD5",
+  normal: "#BF9762",
+  poison: "#C5A9C4",
+  psychic: "#F3A9A9",
+  rock: "#D2CAB3",
+  steel: "#C8C8C8",
+};
+
 export const PokemonDetailsStyle = styled.div`
   width: 100vw;
   height: auto;
@@ -23,45 +44,43 @@ export const BackButton = styled.p`
   position: absolute;
   left: 3%;
 
-  &:hover{
+  &:hover {
     cursor: pointer;
-    transition: all .2s ease-in-out;
+    transition: all 0.2s ease-in-out;
   }
 `;
 
 export const AddButton = styled.button`
-display: flex;
-width: 16rem;
-height: 3.5rem;
-padding: 4px 10px;
-justify-content: center;
-align-items: center;
-flex-shrink: 0;
-border-radius: 0.5rem;
-background: #FF6262;
-border: none;
-color: #FFF;
-font-family: "Poppins";
-font-size: 1rem;
-font-style: normal;
-font-weight: 400;
-line-height: normal;
+  display: flex;
+  width: 16rem;
+  height: 3.5rem;
+  padding: 4px 10px;
+  justify-content: center;
+  align-items: center;
+  flex-shrink: 0;
+  border-radius: 0.5rem;
+  background: #ff6262;
+  border: none;
+  color: #fff;
+  font-family: "Poppins";
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
 
-&:hover{
-  cursor: pointer;
-}
+  &:hover {
+    cursor: pointer;
+  }
 `;
 //Botões//
 
 export const PokebolaCenter = styled.img`
-z-index: -2;
-position: absolute;
-top: 0;
-center: 0 ;
-width: 58rem;
-
-
-`
+  z-index: -2;
+  position: absolute;
+  top: 0;
+  center: 0;
+  width: 58rem;
+`;
 
 export const MainContainer = styled.main`
   display: flex;
@@ -69,79 +88,204 @@ export const MainContainer = styled.main`
   flex-wrap: wrap;
   width: 100vw;
   height: 80vh;
-  background: #5E5E5E;
+  background: #5e5e5e;
   z-index: -3;
   position: relative;
-
 `;
 
 export const ContainerNameDetails = styled.div`
-
-width: 96vw;
-height: 10vh;
-display: flex;
-justify-content: start;
-align-items: center;
-
-h1{
-    color: white;
-  }
-
-`
-
-export const ContainterDetails = styled.div`
-display: flex;
-width: 96vw;
-height: 60vh;
-justify-content: space-around;
-background-color: yellow;
-position: relative;
-z-index: -2;
-border-radius: 2rem;
-`
-export const PokebolaImg = styled.img`
-z-index: -1;
-position: absolute;
-top: 0;
-right: 0 ;
-width: 46rem;
-`
-
-export const ImageAside = styled.aside`
-  height: 60vh;
-  width: 20vw;
-  border: 1px solid black;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-evenly;
-`;
-export const PokemonImage = styled.div`
-  height: 20vh;
-  width: 15vw;
-  border: 1px solid black;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-export const StatsSection = styled.section`
-  height: 60vh;
-  width: 20vw;
-  border: 1px solid black;
-`;
-export const TopicHeader = styled.div`
+  width: 96vw;
   height: 10vh;
   display: flex;
-  justify-content: center;
+  justify-content: start;
   align-items: center;
+
+  h1 {
+    color: white;
+  }
 `;
 
-export const TypesAside = styled.aside`
+export const ContainerDetails = styled.div`
+  display: flex;
+  width: 80vw;
   height: 60vh;
-  width: 30vw;
-  border: 1px solid black;
+  justify-content: space-around;
+  align-items: center;
+  background-color: ${(props) => getBackground[props.type]};
+  position: relative;
+  z-index: -2;
+  border-radius: 2rem;
+`;
+export const PokebolaImg = styled.img`
+  z-index: -1;
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 46rem;
+`;
+
+export const ImageAside = styled.aside`
+  height: 58vh;
+  width: 15vw;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-evenly;
+  /* background-color: green; */
+`;
+export const PokemonImage = styled.div`
+  height: 26vh;
+  width: 12vw;
+  border: 2px solid #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #ffffff;
+  border-radius: 0.5rem;
+
+  img {
+    min-height: 15vh;
+    height: 25vh;
+  }
+`;
+export const StatsSection = styled.section`
+  height: 54vh;
+  width: 20vw;
+  border: 2px #fff black;
+  background-color: #fff;
+  border-radius: 0.5rem;
+  flex-shrink: 0;
+`;
+export const TopicHeader = styled.div`
+  height: 6vh;
+  width: 18vw;
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  display: block;
+  position: relative;
+
+  h2 {
+    position: absolute;
+    left: 3%;
+    font-size: 2rem;
+    font-weight: 800;
+    font-family: Inter;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+  }
+`;
+
+export const AtributesPokemon = styled.div`
+  height: 20vh;
+  margin-left: 1rem;
+  display: flex;
+  flex-direction: column;
+  row-gap: 0.4rem;
+`;
+export const TypesAside = styled.aside`
+  height: 54vh;
+  width: 28vw;
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  justify-content: space-between;
+  position: relative;
+`;
+
+
+export const ContainerDetailsTop = styled.div`
+  width: 28vw;
+  height: 15vh;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const ContainerDetailsNameId = styled.div`
+  width: 13vw;
+  height: 15vh;
+  display: flex;
+  flex-direction: column;
+  margin-left: 1rem;
+
+  p {
+    color: #fff;
+
+    font-family: "Inter";
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+  }
+
+  h2 {
+    font-size: 3rem;
+    font-weight: 800;
+    color: #fff;
+    font-family: Inter;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+  }
+`;
+export const ContainerDetailsImagePokemon = styled.div`
+  width: 13vw;
+  height: 15vh;
+  position: relative;
+
+  img {
+    height: 22vh;
+    position: absolute;
+    right: -3vw;
+    top: -13vh;
+  }
+`;
+
+export const ContainerMoves = styled.div`
+  background-color: white;
+  height: 40vh;
+  width: 15vw;
+  border-radius: 0.5rem;
+
+  h2 {
+    color: #000;
+    font-family: "Inter";
+    font-size: 2rem;
+    font-style: normal;
+    font-weight: 800;
+    line-height: normal;
+  }
+`;
+
+export const DetailsMoves = styled.div`
+  margin-left: 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  row-gap: 1rem;
+  height: 28vh;
+
+  div {
+    width: 10vw;
+    height: 30vh;
+  }
+
+  p {
+    display: inline-flex;
+    min-width: 4vw;
+    padding: 10px;
+    align-items: flex-start;
+    gap: 10px;
+    border-radius: 12px;
+    border: 1px dashed rgba(0, 0, 0, 0.14);
+    background: #ececec;
+    margin-bottom: 0.5rem;
+    color: #000;
+    font-family: monospace, Arial, Helvetica, sans-serif;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  }
 `;
